20bd0a66436be0e642a1953ccdd3f40d4d8277e8
lizechous <lizziepastenes@gmail.com>
2020-11-09 23:29:23 -0300

    creation RegistroReceta model

--- apps/RegistroReceta/admin.py
+++ apps/RegistroReceta/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Receta
+
+# Register your models here.
+admin.site.register(Receta)
+
+
--- apps/RegistroReceta/models.py
+++ apps/RegistroReceta/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Receta(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    nombre_receta = models.TextField()
+    porciones = models.IntegerField()
+    tipo_receta = models.CharField(max_length=30)
+    ingredientes = models.TextField()
+    instrucciones = models.TextField()
+    fecha_creacion =  models.DateTimeField(auto_now_add=True, auto_now=False)
+    imagen = models.ImageField(upload_to='RegistroReceta/img/')
+
+    def __str__(self):
+        return self.nombre_receta
+    
