17f84ab7d0d4dadc192163f80c67fb7d133ce6ba
lizechous <lizziepastenes@gmail.com>
2020-11-11 15:29:59 -0300

    creacion de recetas

--- apps/RegistroReceta/forms.py
+++ apps/RegistroReceta/forms.py
@@ -2,25 +2,24 @@ from django import forms
 from .models import Receta
 
 
-# class RecetaForm(forms.ModelForm):
-#     class Meta:
-#         model = Receta
-#         fields = ['nombre_receta', 'porciones', 'tipo_receta', 'ingredientes', 'instrucciones', 'imagen']
-#         labels = {
-#             'nombre_receta': 'Receta',
-#             'porciones': 'Porciones',
-#             'tipo_receta': 'Tipo receta',
-#             'ingredientes': 'Ingredientes',
-#             'instrucciones': 'Instrucciones',
-#             'imagen': 'Imagen Receta'
+class RecetaForm(forms.ModelForm):
+    class Meta:
+        model = Receta
+        fields = ['nombre_receta', 'porciones', 'tipo_receta', 'ingredientes', 'instrucciones', 'imagen']
+        labels = {
+            'nombre_receta': 'Receta',
+            'porciones': 'Porciones',
+            'tipo_receta': 'Tipo receta',
+            'ingredientes': 'Ingredientes',
+            'instrucciones': 'Instrucciones',
+            'imagen': 'Imagen Receta'
             
 
-#         }
-#         widgets = {
-#             'nombre_receta': forms.TextInput(attrs={'class': 'form-control'}),
-#             'porciones': forms.TextInput(attrs={'class': 'form-control'}),
-#             'tipo_receta': forms.TextInput(attrs={'class': 'form-control'}),
-#             'ingredientes': forms.TextInput(attrs={'class': 'form-control'}),
-#             'instrucciones': forms.TextInput(attrs={'class': 'form-control'}),
-#             'imagen': forms.FileInput(attrs={'class': 'form-control'}),
-#         }
+        }
+        widgets = {
+            'nombre_receta': forms.TextInput(attrs={'class': 'form-control'}),
+            'porciones': forms.NumberInput(attrs={'class': 'form-control'}),
+            'tipo_receta': forms.TextInput(attrs={'class': 'form-control'}),
+            'ingredientes': forms.Textarea(attrs={'class': 'form-control'}),
+            'instrucciones': forms.Textarea(attrs={'class': 'form-control'}),
+        }
--- apps/RegistroReceta/urls.py
+++ apps/RegistroReceta/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path, include
 from . import views
 
-urlpatterns = [
-    
-    path('crear-receta', views.crear_receta, name="crear-receta"),
-    
 
+
+
+urlpatterns = [   
+        # agregar receta
+    path('crearReceta', views.crear_receta, name="crear_receta"),
+        # listar las carreras de la bd
+    path('listarRecetas', views.listar_recetas, name="listar_recetas"),
+    # editar receta
+    path('editarReceta/<int:receta_id>', views.editar_receta ,name="editar_receta"),
+    # borrar receta
+    path('borrarReceta/<int:receta_id>', views.borrar_receta, name="borrar_receta"),
+    
+    path('index', views.index, name="index"),
+    path('registro', views.registro, name="registro"),
+    path('contacto', views.contacto, name="contacto"),
+    path('ingresar', views.ingresar, name="ingresar"),
 ]
 
