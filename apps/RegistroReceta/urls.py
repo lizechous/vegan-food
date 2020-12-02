14c896cda479038f4fbaf0e82866822a0643148f
Nicolás <wantobelieve@gmail.com>
2020-12-02 19:14:45 -0300

    fix index

--- apps/RegistroReceta/urls.py
+++ apps/RegistroReceta/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     # ver receta
     path('verReceta/<int:receta_id>', views.ver_receta, name="ver_receta"),
     
-    path('index', views.index, name="index"),
+    path('', views.index, name="index"),
     path('registro', views.registro, name="registro"),
     path('contacto', views.contacto, name="contacto"),
     path('ingresar', views.ingresar, name="ingresar"),
